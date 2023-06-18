e1872f81d587d917c02998262a96c402e4ffc04a
Timon Claeys <timon.claeys2@student.odisee.be>
2023-06-18 15:21:55 +0200

    updated artist model

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Artist extends Model
 {
     use HasFactory;
+
+    public function songs(): BelongsToMany
+    {
+        return $this->belongsToMany(Song::class, 'artist_song');
+    }
 }
